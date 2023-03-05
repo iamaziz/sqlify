78dfcb21b48841762d4ac9fad50a85e4034f16d8
Aziz Alto <iamaziz.alto@gmail.com>
2023-03-04 19:24:33 -0500

    Introduce Python script execution to manipulate data + Add new sample datasets

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ SAMPLE_DATA = {
     "Periodic Table": "https://gist.githubusercontent.com/GoodmanSciences/c2dd862cd38f21b0ad36b8f96b4bf1ee/raw/1d92663004489a5b6926e944c1b3d9ec5c40900e/Periodic%2520Table%2520of%2520Elements.csv",
     "Movies": "https://raw.githubusercontent.com/reisanar/datasets/master/HollywoodMovies.csv",
     "Iris Flower": "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv",
+    "World Population": "https://gist.githubusercontent.com/curran/13d30e855d48cdd6f22acdf0afe27286/raw/0635f14817ec634833bb904a47594cc2f5f9dbf8/worldcities_clean.csv",
+    "World Cities": "https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/csv/cities.csv",
+    "World States": "https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/csv/states.csv",
+    "World Countries": "https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/csv/countries.csv"
 }
 
 
@@ -71,26 +75,30 @@ def display(df):
         st.sidebar.write(df_)
 
 
-def code_editor():
+def code_editor(language, hint):
     # Spawn a new Ace editor
-    col1, col2 = st.columns([4, 1])
+    col1, col2 = st.columns([2, 1])
+
     with col2:
-        # configs
-        _THEMES = stace.THEMES
-        _KEYBINDINGS = stace.KEYBINDINGS
-        theme = st.selectbox("Theme", options=[_THEMES[2]] + _THEMES)
-        keybinding = st.selectbox(
-            "Keybinding", options=[_KEYBINDINGS[-2]] + _KEYBINDINGS
-        )
-        font_size = st.slider("Font size", min_value=5, max_value=24, value=14)
-        tab_size = st.slider("Tabe size", min_value=1, max_value=8, value=4)
-        # kwargs = {theme: theme, keybinding: keybinding} # TODO: DRY
+        st.write("")
+        with st.expander("EDITOR PANEL"):
+            # configs
+            _THEMES = stace.THEMES
+            _KEYBINDINGS = stace.KEYBINDINGS
+            col21, col22 = st.columns(2)
+            with col21:
+                theme = st.selectbox("Theme", options=[_THEMES[2]] + _THEMES, key=f"{language}1")
+                tab_size = st.slider("Tab size", min_value=1, max_value=8, value=4, key=f"{language}4")
+            with col22:
+                keybinding = st.selectbox("Keybinding", options=[_KEYBINDINGS[-2]] + _KEYBINDINGS, key=f"{language}2")
+                font_size = st.slider("Font size", min_value=5, max_value=24, value=14, key=f"{language}3")
+            # kwargs = {theme: theme, keybinding: keybinding} # TODO: DRY
     with col1:
-        hint = "Type SQL to query the table above. By default, the table name is 'df'. For example, to select 10 rows: \nSELECT * FROM df LIMIT 10"
         content = stace.st_ace(
-            language="sql",
-            height=350,
-            annotations="ANNOOO ",
+            language=language,
+            height=110,
+            show_gutter=False,
+            # annotations="",
             placeholder=hint,
             keybinding=keybinding,
             theme=theme,
@@ -136,6 +144,17 @@ def display_results(query, result: pd.DataFrame):
     download(result)
 
 
+def run_python_script(user_script):
+    py = f"st.write({user_script})"
+    # if py:
+    try:
+        st.write(f"> _RESULTS_")
+        exec(py)
+    except Exception as e:
+        st.warning("Wrong Python command.")
+        # st.exception(e)
+
+
 @st.cache_resource
 def data_profiler(df):
     return ProfileReport(df, title="Profiling Report")
@@ -144,7 +163,7 @@ def data_profiler(df):
 def docs():
     content = """
     
-    #### Query tabular files (csv, xlsx .. etc) using SQL
+    #### Query tabular files (csv, xlsx .. etc) using SQL and/or Python (pandas)
     To get started, drag and drop the file or read from a URL. 
     > <sub>[src code here](https://github.com/iamaziz/sqlify)</sub>
     """
@@ -158,12 +177,31 @@ if __name__ == "__main__":
     df = read_data()
     display(df)
     st.write("---")
-    sql = code_editor()
+    hint = "Type SQL to query the table above. By default, the table name is 'df'. For example, to select 10 rows: \nSELECT * FROM df LIMIT 10"
+    sql = code_editor("sql", hint)
     if sql:
         res = query_data(sql, df)
         display_results(sql, res)
 
+    # run and dexectue python script
+    st.write("---")
+    hint = """Type Python command (one-liner) to execute or manipulate the dataframe e.g. `df.sample(7)`. Results rendered using `st.write()`.
+    📊 Visulaization example: from "movies" dataset, plot average rating by genre:
+        st.line_chart(df.groupby("Genre")[["RottenTomatoes", "AudienceScore"]].mean())
+    🗺 Maps example: show the top 10 populated cities in the world on map (from "Cities Population" dataset)
+        st.map(df.sort_values(by='population', ascending=False)[:10])
+    """
+
+    user_script = code_editor("python", hint)
+    if user_script:
+        # st.map(df.sample(10)[["lat", "lng"]])
+        df.rename(columns={"lng":"lon"}, inplace=True)
+        run_python_script(user_script)
+
+    st.write("---")
     if st.checkbox("DATA PROFILE"):
         profile = data_profiler(df)
 
         st_profile_report(profile)
+
+    st.write("---")
